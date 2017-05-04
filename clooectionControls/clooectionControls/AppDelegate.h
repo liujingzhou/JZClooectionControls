c6522b47195d286809048c2944a4a639373e52f2
tcyx <tcyx@tcyxdeMacBook-Pro.local>
2017-05-04 09:53:42 +0800

    创建工程

---
+++ clooectionControls/clooectionControls/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  clooectionControls
+//
+//  Created by tcyx on 2017/5/4.
+//  Copyright © 2017年 ljz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ clooectionControls/clooectionControls/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  clooectionControls
+//
+//  Created by tcyx on 2017/5/4.
+//  Copyright © 2017年 ljz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
